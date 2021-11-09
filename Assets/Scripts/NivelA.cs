d817438a6d9096386937801120194110ffa195d1
Bruno Olaso Alaya <60631070+BlueOli@users.noreply.github.com>
2021-11-09 18:58:39 -0300

    Plataformitas
    +Plataformas (Back&Forth)
    +NivelA Completado!

---
+++ Assets/Scripts/NivelA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NivelA : MonoBehaviour
+{
+    public Light lightA;
+    public Light lightB;
+    public Light lightC;
+    public Light lightD;
+    public Light lightE;
+    public Light lightF;
+    public bool levelPassed;
+    public Animator myAnimationController;
+ 
+    void Update()
+    {
+        if(!levelPassed && lightA.enabled && lightB.enabled && lightC.enabled && lightD.enabled && lightE.enabled && lightF.enabled)
+        {
+            levelPassed = !levelPassed;
+            myAnimationController.SetBool("Completado", true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PasajeArco.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasajeArco : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (!GetComponent<Light>().enabled) {
+                GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+            }
+        }
+    }
+
+}
