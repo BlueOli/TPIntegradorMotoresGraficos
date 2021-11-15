3744abd2fa3bf91be866cc08881c537453f7266a
Bruno Olaso Alaya <60631070+BlueOli@users.noreply.github.com>
2021-11-15 12:41:28 -0300

    Nivel E Completado!
    +NivelE
    +LuzCentroE
    +RayCasting! Torus

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -3,6 +3,8 @@
 public class ControlJugador : MonoBehaviour
 {
     public float rapidezDesplazamiento = 10.0f;
+    
+    public Camera camaraPrimeraPersona;
 
     void Start()
     {
@@ -23,5 +25,39 @@ public class ControlJugador : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.None;
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camaraPrimeraPersona.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            if ((Physics.Raycast(ray, out hit) == true))
+            {
+                Debug.Log("El rayo tocó al objeto: " + hit.collider.tag);
+                if(hit.collider.tag == "TP1")
+                {
+                    transform.position = new Vector3(527f, 90f, 125f);
+                }
+            }
+
+            if ((Physics.Raycast(ray, out hit) == true))
+            {
+                Debug.Log("El rayo tocó al objeto: " + hit.collider.tag);
+                if (hit.collider.tag == "TP2")
+                {
+                    transform.position = new Vector3(515f, 99f, 156f);
+                }
+            }
+
+            if ((Physics.Raycast(ray, out hit) == true))
+            {
+                Debug.Log("El rayo tocó al objeto: " + hit.collider.tag);
+                if (hit.collider.tag == "TP3")
+                {
+                    transform.position = new Vector3(558f, 109f, 168f);
+                }
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/NivelE.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NivelE : MonoBehaviour
+{
+    public GameObject nivelE;
+    public Animator myAnimationControllerNivelE;
+    public Animator myAnimationController;
+    bool levelIniciado = false;
+    public bool levelPassed = false;
+    public Light lightA;
+    public Light lightB;
+    public Light lightC;
+    public Light lightD;
+    public Light lightE;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (!levelIniciado)
+        {
+            if (col.CompareTag("Player"))
+            {
+                levelIniciado = !levelIniciado;
+                myAnimationControllerNivelE.SetBool("Iniciado", true);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!levelPassed && lightA.enabled && lightB.enabled && lightC.enabled && lightD.enabled && lightE.enabled)
+        {
+            levelPassed = !levelPassed;
+            myAnimationController.SetBool("Completado", true);
+        }
+    }
+
+}
