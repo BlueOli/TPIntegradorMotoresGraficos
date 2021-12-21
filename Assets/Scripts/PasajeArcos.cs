e99365f96514977716ab83bacba1b213db175750
Bruno Olaso Alaya <60631070+BlueOli@users.noreply.github.com>
2021-12-21 18:57:56 -0300

    CommitFinal
    +Mejora Nivel A

---
+++ Assets/Scripts/PasajeArcos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasajeArcos : MonoBehaviour
+{
+    public Light refLight;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (!GetComponent<Light>().enabled && refLight.enabled)
+            {
+                GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+            }
+        }
+    }
+
+}
